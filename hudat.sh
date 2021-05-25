d65b0a244a27cc968cedb1e6b483778663249f1e
Lennie Budgell <lbudgell@gmail.com>
2021-05-25 17:38:34 -0600

    Update hudat.sh
    Fixed a typo

--- hudat.sh
+++ hudat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo systemctl restart wpa_supplicant.service networking.service && updown && echo 'Network MAGIC spells cast.' && echo  '...HUDAT!..'  && ba``sh -c './start.sh' && tail -f screenlog.0
+sudo systemctl restart wpa_supplicant.service networking.service && updown && echo 'Network MAGIC spells cast.' && echo  '...HUDAT!..'  && bash -c './start.sh' && tail -f screenlog.0
